4fc5e5bbd133a975dd12372b61187a4d922cacc6
liweiyang <15658150268@163.com>
2019-09-12 09:22:46 +0800

    初始化

---
+++ src/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+import '../node_modules/font-awesome/css/font-awesome.min.css'
+import './index.css';
+import './index.scss';
+
+
+ReactDOM.render(
+    <div>
+        <h1>Hello, world!</h1>
+    </div>,
+    document.getElementById('app')
+);
